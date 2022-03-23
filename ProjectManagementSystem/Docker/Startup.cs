7647e5079b5030282aae963cb233f522f10d77fe
vtakdeniz <vtakdeniz@gmail.com>
2022-03-23 14:40:11 +0300

    Updated docker files

---
+++ ProjectManagementSystem/Docker/Startup.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagementSystem.Data;
+using AutoMapper;
+using Newtonsoft.Json.Serialization;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using ProjectManagementSystem.Models.UserElements;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
+namespace ProjectManagementSystem
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Project Management System Api",
+                    Description = "Project Management System Api",
+                    //TermsOfService = new Uri(""),
+                });
+            });
+
+            services.AddDbContext<ManagementContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("ProjectManagementSystemConnection")));
+            services.AddControllers();
+
+            services.AddControllers().AddNewtonsoftJson(s => {
+                s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            });
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ManagementContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(options =>
+                {
+                    options.SaveToken = true;
+                    options.RequireHttpsMetadata = false;
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
+                    };
+                });
+
+
+
+
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                
+                app.UseCors(x => x
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .SetIsOriginAllowed(origin => true) 
+                    .AllowCredentials());
+
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Project Management System v1");
+                });
+
+                app.UseDeveloperExceptionPage();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+
+            app.UseAuthentication();
+           
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ ProjectManagementSystem/Docker/setDependencies.sh
@@ -0,0 +1,19 @@
+dotnet add package Microsoft.EntityFrameworkCore.Design  --version 5.0.12 ;
+
+dotnet add package Microsoft.EntityFrameworkCore --version 5.0.12 ;
+
+dotnet add package Microsoft.AspNetCore.Mvc.NewtonsoftJson  --version 5.0.12 ;
+
+dotnet add package Microsoft.AspNetCore.JsonPatch --version 5.0.12 ;
+
+dotnet add package AutoMapper.Extensions.Microsoft.DependencyInjection --version 5.0.12 ;
+
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer --version 5.0.12 ; 
+
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design --version 5.0.2 ;   
+
+dotnet add package Microsoft.AspNetCore.Identity
+
+dotnet add package Microsoft.AspNetCore.Authentication.JwtBearer --version 5.0.14
+
+dotnet add package Microsoft.AspNetCore.Identity.EntityFrameworkCore --version 5.0.10
