9fe0c674974130942aefdf2326c171b7d83efd83
vtakdeniz <vtakdeniz@gmail.com>
2022-05-10 17:57:07 +0300

    Fixed cascade delete bug

---
+++ ProjectManagementSystem/Migrations/20220510140303_new.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagementSystem.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_boardHasTeams_boards_board_id",
+                table: "boardHasTeams");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_boardHasTeams_boards_board_id",
+                table: "boardHasTeams",
+                column: "board_id",
+                principalTable: "boards",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_boardHasTeams_boards_board_id",
+                table: "boardHasTeams");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_boardHasTeams_boards_board_id",
+                table: "boardHasTeams",
+                column: "board_id",
+                principalTable: "boards",
+                principalColumn: "Id");
+        }
+    }
+}
