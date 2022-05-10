6b49585973f7c0eecb45ac620134a064a134165d
vtakdeniz <vtakdeniz@gmail.com>
2022-05-10 18:14:35 +0300

    Changed version number

--- ProjectManagementSystem/Startup.cs
+++ ProjectManagementSystem/Startup.cs
@@ -40,7 +40,7 @@ namespace ProjectManagementSystem
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
-                    Version = "v2.0.5",
+                    Version = "v2.0.6",
                     Title = "Project Management System Api",
                     Description = "Project Management System Api",
                     //TermsOfService = new Uri(""),
